fa8150b014ac45f34729966e530e6373c37ac27e
zhanghongsheng <zhanghongshengs5@163.com>
2026-06-27 20:18:32 +0800

    Bump web admin assets for alias limit deploy

--- tests/web-admin/static.test.js
+++ tests/web-admin/static.test.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const WEB_ADMIN_ASSET_VERSION = '20260627-participant-remark-20';
+const WEB_ADMIN_ASSET_VERSION = '20260627-manager-alias-40';
 
 test('web admin static shell defaults to Chinese visible copy', () => {
   const html = fs.readFileSync(path.join(process.cwd(), 'web-admin/index.html'), 'utf8');
